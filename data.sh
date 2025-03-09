eba9690242a67865cce00232a69249b218f76bc6
karanshahh <karanshah.cs@gmail.com>
2025-03-09 01:05:55 -0600

    simple line for time_id=5

--- data.sh
+++ data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-kaggle competitions download -c optiver-realized-volatility-prediction -d input/optiver-realized-volatility-prediction
-mv optiver-realized-volatility-prediction.zip input/optiver-realized-volatility-prediction
-unzip input/optiver-realized-volatility-prediction/optiver-realized-volatility-prediction.zip -d input/optiver-realized-volatility-prediction/
-rm input/optiver-realized-volatility-prediction/optiver-realized-volatility-prediction.zip
+kaggle competitions download -c optiver-realized-volatility-prediction -d input/
+mv optiver-realized-volatility-prediction.zip input/
+unzip input/optiver-realized-volatility-prediction.zip -d input/
+rm input/optiver-realized-volatility-prediction.zip
