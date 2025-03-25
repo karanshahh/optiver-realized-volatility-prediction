70b2a9cc70650331c20787487a9913f61c195c9d
karanshahh <karanshah.cs@gmail.com>
2025-03-24 23:49:29 -0500

    updated submission.csv file

--- data.sh
+++ data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-kaggle competitions download -c optiver-realized-volatility-prediction -d input/
-mv optiver-realized-volatility-prediction.zip input/
+kaggle competitions download -c optiver-realized-volatility-prediction
+mkdir input/ && mv optiver-realized-volatility-prediction.zip input/
 unzip input/optiver-realized-volatility-prediction.zip -d input/
 rm input/optiver-realized-volatility-prediction.zip
